9fd89c7e1f7999b4794e1c605d5aac717b5ae8d1
Monika <mondrzejewska.m@gmail.com>
2026-05-17 13:07:46 +0200

    Usuwanie spotkań

--- src/main/frontend/src/meetings/MeetingsPage.js
+++ src/main/frontend/src/meetings/MeetingsPage.js
@@ -29,9 +29,15 @@ export default function MeetingsPage({username}) {
         }
     }
 
-    function handleDeleteMeeting(meeting) {
-        const nextMeetings = meetings.filter(m => m !== meeting);
-        setMeetings(nextMeetings);
+    async function handleDeleteMeeting(meeting) {
+        const response = await fetch(`/api/meetings/${meeting.id}`, {
+            method: 'DELETE'
+        });
+
+        if (response.ok) {
+            const nextMeetings = meetings.filter(m => m.id !== meeting.id);
+            setMeetings(nextMeetings);
+        }
     }
 
 
